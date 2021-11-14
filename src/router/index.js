116c9bb4170075760f0f7687623d0980eca88368
tixdim <rzhuludkov@bk.ru>
2021-11-14 17:54:42 +0300

    123

--- src/router/index.js
+++ src/router/index.js
@@ -76,7 +76,20 @@ export const constantRoutes = [
       }
     ]
   },
-
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
+  },
   {
     path: '/form',
     component: Layout,
